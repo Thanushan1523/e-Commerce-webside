5a7dbd5ce89ec2a4372f43b2db68265005682867
Thanushan1523 <thanushan1523@gmail.com>
2023-11-04 06:59:56 -0400

    app-item

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name= models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+ 
+
+  
+    def __str__(self) :
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
