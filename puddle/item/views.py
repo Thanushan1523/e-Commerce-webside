77744b037fb4f14d2427d80657f40560b3207efa
Thanushan1523 <thanushan1523@gmail.com>
2023-11-05 08:25:33 -0500

    item detail  page

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -4,4 +4,4 @@ from .models import Item
 def detail(request,pk):
         item =get_object_or_404(Item ,pk=pk)
 
-        return render(request , 'item/detail.html', {'item:item'})
+        return render(request , 'item/detail.html', {'item':item})
