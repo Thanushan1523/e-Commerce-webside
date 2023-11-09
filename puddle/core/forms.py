d24aa7cc748015141da6c5555282277b33c78a1c
Thanushan1523 <thanushan1523@gmail.com>
2023-11-08 21:39:15 -0500

    siginup page - forms

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -7,5 +7,19 @@ class SignupForm(UserCreationForm):
         model =User
         fields = ('username', 'email', 'password1', 'password2')
 
-    username =forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Your username'}))
+    username =forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Your username',
+                                                            'class': 'w-full py-4 px-6 rounded-xl'
+                                                            }))
+    
+    email =forms.CharField(widget=forms.EmailInput(attrs={'placeholder' : 'Your Email address',
+                                                            'class': 'w-full py-4 px-6 rounded-xl'
+                                                            }))
+    
+    password1 =forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'password',
+                                                            'class': 'w-full py-4 px-6 rounded-xl'
+                                                            }))
+    
+    password2 =forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Repeat password',
+                                                            'class': 'w-full py-4 px-6 rounded-xl'
+                                                            }))
     
