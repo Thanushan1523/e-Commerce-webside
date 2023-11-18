16b0b0888f6dcac29a85e14387182cb6068b91a3
Thanushan1523 <thanushan1523@gmail.com>
2023-11-18 15:01:04 -0500

    adding item

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render ,get_object_or_404
+
 from .models import Item
+from .forms import NewItemForm
 # Create your views here.
 def detail(request,pk):
         item =get_object_or_404(Item ,pk=pk)
@@ -9,3 +12,12 @@ def detail(request,pk):
                                'related_items':related_items                      
                                                 
                                                      })
+
+
+@login_required
+def new(request):
+        form= NewItemForm()
+
+        return render(request,'item/form.html',{
+                'form': form
+        })
