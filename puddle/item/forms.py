ddc6bcc7deb440ce3ebe1ae2f3854ba495f6412f
Thanushan1523 <thanushan1523@gmail.com>
2023-11-19 13:27:49 -0500

    adding item

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -4,4 +4,4 @@ from .models import Item
 class NewItemForm (forms.ModelForm):
     class Meta:
         model=Item
-        fields =('category' ,'name','description','price','image,')
+        fields =('category' ,'name','description','price','image',)
