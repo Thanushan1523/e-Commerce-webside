ee1028eae127d27aa8238923bcde460007f9807e
Thanushan1523 <thanushan1523@gmail.com>
2023-11-21 07:32:43 -0500

    dashboard

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -3,3 +3,9 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from item.models import Item
 
+@login_required
+def index(request):
+    items =Item.objects.filter(created_by=request.user)
+    return render(request,'dashboard/index.html',{
+        'items':items,
+    })
