a4627bbe9d94ac05e656f9e3bb09b916e3a7b825
Thanushan1523 <thanushan1523@gmail.com>
2023-11-22 08:01:26 -0500

    editing item

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render ,get_object_or_404 ,redirect
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 # Create your views here.
 def detail(request,pk):
         item =get_object_or_404(Item ,pk=pk)
@@ -27,8 +27,27 @@ def new(request):
                 form= NewItemForm()
 
         return render(request,'item/form.html',{
-                'form': form
+                'form': form,
+                'title': 'New Item',
         })
+@login_required
+def edit(request,pk):
+        item = get_object_or_404(Item ,pk=pk ,created_by=request.user)
+        if request.method == 'POST':
+              form= EditItemForm(request.POST, request.FILES)   
+              if form.is_valid():
+                     item=form.save(commit=False)
+                     item.created_by =request.user
+                     item.save()
+                     return redirect ('item:detail',pk=item.id)
+        else:
+                form= EditItemForm()
+
+        return render(request,'item/form.html',{
+                'form': form,
+                'title': 'Edit Item',
+        })
+
 
 
 @login_required
@@ -36,3 +55,4 @@ def delete(request,pk):
     item = get_object_or_404(Item ,pk=pk ,created_by=request.user)
     item.delete()
     return redirect('dashboard:index')
+
