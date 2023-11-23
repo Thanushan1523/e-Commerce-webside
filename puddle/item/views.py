fd81d95f7d1c14779db8f50e7317aacf1fe7d5b1
Thanushan1523 <thanushan1523@gmail.com>
2023-11-23 07:48:39 -0500

    browse item

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render ,get_object_or_404 ,redirect
 
 from .models import Item
 from .forms import NewItemForm, EditItemForm
+
 # Create your views here.
+
+def items(request):
+       item=item.objects.filter(is_sold=False)
+       return render(request , 'item/item.html', {'items':items,}) 
 def detail(request,pk):
         item =get_object_or_404(Item ,pk=pk)
         related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
