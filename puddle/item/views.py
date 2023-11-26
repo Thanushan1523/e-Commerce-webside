bab8bf0848e12ec390ee6d52b0403d4b39b1df52
Thanushan1523 <thanushan1523@gmail.com>
2023-11-26 14:55:16 -0500

    developing the slide bar in browse

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -7,8 +7,12 @@ from .forms import NewItemForm, EditItemForm
 # Create your views here.
 
 def items(request):
+       query= request.GET.get('query', '')
        items=Item.objects.filter(is_sold=False)
-       return render(request , 'item/items.html', {'items':items,}) 
+       if query:
+              items =items.filter(name__icontains=query)
+       return render(request , 'item/items.html', {'items':items, 
+                                                   'query': query,}) 
 def detail(request,pk):
         item =get_object_or_404(Item ,pk=pk)
         related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
